922cb4a061220f13dc43c147416f904f16ee2120
Hayato AKAGIRI <87093hayato@gmail.com>
2026-04-23 08:13:34 +0900

    feat: Work エンティティと IndexedDB リポジトリの実装
    - Work エンティティに htmlFileUrl / htmlFileCharset フィールドを追加
    - workCatalogRepository で html_file_url / html_file_charset をマッピング
    - workIndexedDbRepository を新規実装（保存・取得・削除・閲覧位置管理）

--- src/data/repositories/workCatalogRepository.ts
+++ src/data/repositories/workCatalogRepository.ts
@@ -6,16 +6,26 @@ export function getWorkCatalog(): Work[] {
 
   return dataset
     .map((item) => {
+      // 著者名は姓と名を結合して生成する
+      const lastName = readString(item, ["last_name", "author", "著者名"]);
+      const firstName = readString(item, ["first_name"]);
+      const author =
+        lastName && firstName
+          ? `${lastName} ${firstName}`
+          : lastName ?? firstName;
+
       const candidate = {
-        id: readString(item, ["id", "作品ID"]),
+        id: readString(item, ["work_id", "id", "作品ID"]),
         title: readString(item, ["title", "作品名"]),
         subtitle: readString(item, ["subtitle", "サブタイトル"]),
-        originalTitle: readString(item, ["originalTitle", "オリジナルタイトル"]),
-        author: readString(item, ["author", "著者名"]),
-        firstPublishedYear: readString(item, ["firstPublishedYear", "底本初版発行年"]),
-        writingStyle: readString(item, ["writingStyle", "文字遣い種別"]),
-        publisher: readString(item, ["publisher", "底本親本出版社"]),
-        sourceBookName: readString(item, ["sourceBookName", "底本名"]),
+        originalTitle: readString(item, ["original_title", "originalTitle", "オリジナルタイトル"]),
+        author,
+        firstPublishedYear: readString(item, ["source_first_edition_year", "firstPublishedYear", "底本初版発行年"]),
+        writingStyle: readString(item, ["orthography_type", "writingStyle", "文字遣い種別"]),
+        publisher: readString(item, ["source_publisher", "publisher", "底本親本出版社"]),
+        sourceBookName: readString(item, ["source_book_name", "sourceBookName", "底本名"]),
+        htmlFileUrl: readString(item, ["html_file_url"]),
+        htmlFileCharset: readString(item, ["html_file_charset"]),
         content: readString(item, ["content", "本文"]),
       };
 
