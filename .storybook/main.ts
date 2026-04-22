17a416a2189f64bd930c95265e2c4bce1c4b792f
Hayato AKAGIRI <87093hayato@gmail.com>
2026-04-22 16:19:39 +0900

    fix dir

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,6 +4,6 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: ["@storybook/addon-a11y", "@storybook/addon-docs", "@storybook/addon-onboarding"],
   framework: "@storybook/nextjs",
-  staticDirs: ["../public"],
+
 };
 export default config;
